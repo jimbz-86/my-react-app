ff72912df2f5dd4b7dd198690ea9ca213ec7671d
jimbz-86 <publicjimbz@gmail.com>
2026-01-05 18:23:48 +0800

    added free code camp code workshops

---
+++ src/free-code-camp/footer.js
@@ -0,0 +1,33 @@
+export  const Footer = () => {
+  return (
+  <footer>
+    <ul>
+      <li>
+        <a href="#">Fitness Dashboard</a>
+      </li>
+      <li>
+        <a href="#">Services</a>
+      </li>
+    </ul>
+      <ul>
+       <li>
+        <a href="#">Watch Videos</a>
+      </li>
+      <li>
+        <a href="#">Discord</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <a href="#">Privacy Policy</a>
+      </li>
+      <li>
+        <a href="#">Terms</a>
+      </li>
+    </ul>
+    <p>
+    © 2024 Fitness Dashboard. All Rights Reserved.
+    </p>
+  </footer>
+  );
+};
